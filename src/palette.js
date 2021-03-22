7defcfd8dd9e68a18053eaca13a16e8f8e4100ba
gordongest <gordon.gest@gmail.com>
2021-03-22 17:03:44 -0600

    added palette component, passed props

---
+++ src/palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="palette">
+        {/* navbar */}
+        <div className="palette-colors">
+        {/* footer */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Palette;
