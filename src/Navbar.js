4c3277a064b9197199657bd5bbb2cd2aece4c801
gordongest <gordon.gest@gmail.com>
2021-03-22 21:45:39 -0600

    styling navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './Navbar.css';
 
 const Navbar = ({ level, setLevel }) => (
   <header className="navbar">
     <div className="logo">
-      <a href="/">Color Palettes</a>
+      <a href="/">colorPalettePicker</a>
     </div>
-    <div className="slider">
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={1000}
-        step={100}
-        onAfterChange={setLevel}
-      />
+    <div className="slider-container">
+      <span>Level: {level}</span>
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={1000}
+          step={100}
+          onAfterChange={setLevel}
+        />
+      </div>
     </div>
   </header>
 );
