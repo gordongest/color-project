8b617438811c758528ea3d45facf2e5ee634317a
gordongest <gordon.gest@gmail.com>
2021-03-22 21:11:00 -0600

    added rc-slider

--- src/palette.js
+++ src/palette.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ColorBox from './ColorBox';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 
-const Palette = ({ colors }) => {
+const Palette = ({ palette }) => {
 
-  const colorBoxes = colors.map(color => <ColorBox background={color.color} name={color.name} />)
+  const [ level, setLevel ] = useState(500);
+
+  const colorBoxes = palette.colors[level].map(color => <ColorBox background={color.hex} name={color.name} />)
 
   return (
     <div className="palette">
+      <Slider
+        defaultValue={level}
+        min={100}
+        max={1000}
+        step={100}
+        onAfterChange={setLevel}
+      />
       {/* navbar */}
       <div className="palette-colors">
         {colorBoxes}
