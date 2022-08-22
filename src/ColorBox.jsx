48d1d4b58f2d88d169d9c14dc8205904441a237c
Gordon Gest <gordon.gest@gmail.com>
2022-08-22 14:39:26 -0600

    style color box and buttons

--- src/ColorBox.jsx
+++ src/ColorBox.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './ColorBox.css';
 
 const ColorBox = ({ background, name }) =>
-    <div className="ColorBox" style={{background: background}}>
-        <span>{name}</span>
+    <div className="ColorBox" style={{ background }}>
+        <div className="copy-container">
+            <div className="box-content">
+                <span>{name}</span>
+            </div>
+            <button className="copy-button">copy</button>
+        </div>
+        <span className="see-more">more</span>
     </div>
 
 export default ColorBox;
