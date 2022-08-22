59fd57eaa7661c55e0b74c6986be52ea151b8655
Gordon Gest <gordon.gest@gmail.com>
2022-08-22 15:04:42 -0600

    set up state, implement handleCopy method, dynamically set class of overlay, add styling

--- src/ColorBox.jsx
+++ src/ColorBox.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CopyToClipboard } from "react-copy-to-clipboard/lib/Component";
 import './ColorBox.css';
 
-const ColorBox = ({ background, name }) =>
-    <div className="ColorBox" style={{ background }}>
-        <div className="copy-container">
-            <div className="box-content">
-                <span>{name}</span>
+const ColorBox = ({ background, name }) => {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const handleCopy = () => {
+        setIsSelected(true);
+        setTimeout(() => setIsSelected(false), 1500);
+    }
+
+    return (
+        <div className="ColorBox" style={{ background }}>
+            <div className={`overlay ${isSelected && 'show'}`} style={{ background }}></div>
+            <div className="copy-container">
+                <div className="box-content">
+                    <span>{name}</span>
+                </div>
+                <CopyToClipboard text={background} onCopy={handleCopy}>
+                    <button className="copy-button">copy</button>
+                </CopyToClipboard>
             </div>
-            <CopyToClipboard text={background}>
-                <button className="copy-button" onClick={()=> console.log('clikke')}>copy</button>
-            </CopyToClipboard>
+            <span className="see-more">more</span>
         </div>
-        <span className="see-more">more</span>
-    </div>
+    )
+}
 
 export default ColorBox;
